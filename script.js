5d4f5e9e1c44c2880a84cc64af4eff66c53b531b
Rajalakshmi Tamilselvan <ts7226987@gmail.com>
2023-03-17 14:33:04 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,106 @@
+function htmlProjects() {
+  let getDiv = document.getElementById("all_contenet");
+  getDiv.innerHTML = `
+  <div id="inside_boxes">
+  <div id="des_div">
+  <p id="des_p">It is a periodic Table.This project is only Alignments.
+  I used to grid properties in this project.
+  </p>
+  </div>
+  <a href="https://comforting-gnome-0c6d61.netlify.app/"  target="_blank"> <img src="periadic.png " id="pre_img"/></a>
+ </div>
+ <div id="inside_boxes">
+ <div id="des_div">
+  <p id="des_p">It is a simple background video.
+  I used to video in this project.
+  I learned and used position properties in this project.
+  </p>
+  </div>
+  <a href="https://background-video.tamilselvan25.repl.co/"  target="_blank"> <img src="bgVideo.png" id="bg_Img"/></a>
+ </div>
+ <div id="inside_boxes">
+ <div id="des_div">
+ <p id="des_p">It is a webpage in alignments.
+ I learned and used display properties in this project.
+ </p>
+ </div>
+ <a href="https://ass11.tamilselvan25.repl.co/"  target="_blank"><img src="webpage.png" id="web_Img"/></a>
+ </div>
+ <div id="inside_boxes">
+ <div id="des_div">
+ <p id="des_p">It is a layout alignments project.
+ I learned and used float and flex properties in this project.
+ I learned this projects correct and neet alignments.
+ </p>
+ </div>
+  <a href="https://layout.tamilselvan25.repl.co/"  target="_blank"> <img src="layouts.png" id="layout_Img"/></a>
+ </div>
+ <div id="inside_boxes">
+ <div id="des_div">
+ <p id="des_p">It is all planet webpage.I learned this page to other pagge nevigation means used a tags and attributes.
+ totally 27 pages nevigation in this project.
+ </p>
+ </div>
+  <a href="https://final.tamilselvan25.repl.co/"  target="_blank"> <img src="planet.jpg" id="planet_Img"/></a>
+ </div>
+ <div id="inside_boxes"></div>`;
+}
+
+function jsProjects() {
+  let getDiv = document.getElementById("all_contenet");
+  getDiv.innerHTML = `
+  <div id="inside_boxes_js">
+  <div id="des_div">
+  <p id="des_p">It is a simple music player page.
+  I used to image in this project.
+  I am created play button clicked play this song,pause button clicked pause this song , next button clicked next song play,
+  prevoius button clicked previous song play in all functions.
+  </p>
+  </div>
+ <a href="https://music-player.tamilselvan25.repl.co/" target="_blank"><img src="musicc.jpg" id="audio_img"/><a>
+  </div>
+  <div id="inside_boxes_js">
+  <a href="https://gleeful-cuchufli-b135d1.netlify.app/"  target="_blank"><img src="job.jpg" id="job_Img"/>
+  </div>
+  <div id="inside_boxes_js">
+  <a href="https://final-project-js.tamilselvan25.repl.co/"  target="_blank"> <img src="magic_logo.jpg" id="invoice_logo"/></a>
+  </div>
+  <div id="inside_boxes_js">
+  <div id="des_div">
+  <p id="des_p">This is  Memorygame .I learned and used to array and objects.
+I used grid properties,floor and random medthos in this game.
+  </p>
+  </div>
+  <a href="https://memory-game.tamilselvan25.repl.co/"  target="_blank"> <img src="memory_game.png" id="memory_game"/></a>
+  </div>
+  <div id="inside_boxes_js">
+  <div id="des_div">
+  <p id="des_p">This is time tracking page.Totally three filters daily,weekly,monthly.I get and used JSON datas.
+  I used to grid properties in this project.
+  </p>
+  </div>
+  <a href="https://subtle-pasca-254621.netlify.app/"  target="_blank"><img src="time track.jpg" </div> id="time_Img"/><a>
+  </div>
+  <div id="inside_boxes_js">
+  <div id="des_div">
+  <p id="des_p">This is a url short page.I used and learned fetch mehod and api.
+  </p>
+  </div>
+   <a href="https://statuesque-blancmange-c9b129.netlify.app/"  target="_blank"> <img src="urlShortLogo.jpg" id="urlshort"/><a>
+  </div>`;
+}
+
+function nodeProjects() {
+  let getDiv = document.getElementById("all_contenet");
+  getDiv.innerHTML = `
+  <div id="inside_boxes_node">
+  <video src="movie.webm" id="in_video" style="width:100%;height:100%" controls> </video>
+  </div>
+  <div id="inside_boxes_node">
+  <video src="invoice.webm" id="in_video" style="width:100%;height:100%" controls> </video>
+  </div>
+  <div id="inside_boxes_node">
+  <a href="https://github.com/Rajalakshmi2005/IP-Address-Tracker"/><img src="ipAddress.jpg" id="en_Img"/><a>
+  </div>
+`;
+}
