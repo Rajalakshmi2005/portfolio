dc7a07cd8e488f7d08b9a169649c3da2adca5427
Rajalakshmi Tamilselvan <ts7226987@gmail.com>
2023-03-21 12:39:46 +0530

    changes

--- script.js
+++ script.js
@@ -1,97 +1,146 @@
 function htmlProjects() {
   let getDiv = document.getElementById("all_contenet");
+
   getDiv.innerHTML = `
-  <div id="inside_boxes">
+  <div id="inside_boxes" style="background-image: url(periadic.png)";>
   <div id="des_div">
   <p id="des_p">It is a periodic Table.This project is only Alignments.
   I used to grid properties in this project.
   </p>
+  <a href="https://comforting-gnome-0c6d61.netlify.app/"  target="_blank"><button   class="btn3 sub_btn" >View</button></a>
   </div>
-  <a href="https://comforting-gnome-0c6d61.netlify.app/"  target="_blank"> <img src="periadic.png " id="pre_img"/></a>
+  
  </div>
- <div id="inside_boxes">
+
+
+ <div id="inside_boxes" style="background-image: url(bgVideo.png); background-size: 100% 100%;">
  <div id="des_div">
   <p id="des_p">It is a simple background video.
+
   I used to video in this project.
   I learned and used position properties in this project.
   </p>
+  <a href="https://background-video.tamilselvan25.repl.co/"  target="_blank"> <button   class="btn3 sub_btn" >View</button></a>
   </div>
-  <a href="https://background-video.tamilselvan25.repl.co/"  target="_blank"> <img src="bgVideo.png" id="bg_Img"/></a>
+  
  </div>
- <div id="inside_boxes">
+
+
+ <div id="inside_boxes" style="background-image: url(webpage.png); background-size: 100% 100%">
  <div id="des_div">
  <p id="des_p">It is a webpage in alignments.
  I learned and used display properties in this project.
  </p>
+ <a href="https://ass11.tamilselvan25.repl.co/"  target="_blank"><button   class="btn3 sub_btn"  >View</button></a>
  </div>
- <a href="https://ass11.tamilselvan25.repl.co/"  target="_blank"><img src="webpage.png" id="web_Img"/></a>
+ 
  </div>
- <div id="inside_boxes">
+
+
+ <div id="inside_boxes" style="background-image: url(layouts.png); background-size: 100% 100%;">
  <div id="des_div">
  <p id="des_p">It is a layout alignments project.
  I learned and used float and flex properties in this project.
  I learned this projects correct and neet alignments.
  </p>
+ <a href="https://layout.tamilselvan25.repl.co/"  target="_blank">  <button   class="btn3 sub_btn" >View</button></a>
+
  </div>
-  <a href="https://layout.tamilselvan25.repl.co/"  target="_blank"> <img src="layouts.png" id="layout_Img"/></a>
+ 
  </div>
- <div id="inside_boxes">
+
+
+ <div id="inside_boxes"  style="background-image:url(planet.jpg); background-size:100% 100%;">
  <div id="des_div">
  <p id="des_p">It is all planet webpage.I learned this page to other pagge nevigation means used a tags and attributes.
  totally 27 pages nevigation in this project.
  </p>
+ <a href="https://final.tamilselvan25.repl.co/"  target="_blank"> <button   class="btn3 sub_btn" >View</button></a>
+
  </div>
-  <a href="https://final.tamilselvan25.repl.co/"  target="_blank"> <img src="planet.jpg" id="planet_Img"/></a>
+ 
  </div>
  <div id="inside_boxes"></div>`;
 }
 
 function jsProjects() {
   let getDiv = document.getElementById("all_contenet");
+
   getDiv.innerHTML = `
-  <div id="inside_boxes_js">
+  <div id="inside_boxes_js" style="background-image:url(musicc.jpg) ;background-size:100% 100%">
   <div id="des_div">
-  <p id="des_p">It is a simple music player page.
+  <p id="des_p">It is a simple music player .
   I used to image in this project.
-  I am created play button clicked play this song,pause button clicked pause this song , next button clicked next song play,
-  prevoius button clicked previous song play in all functions.
+  I am created play button clicked play this song,pause button,next buutton,previous button
   </p>
+  <a href="https://music-player.tamilselvan25.repl.co/"  target="_blank"> <button   class="btn3 sub_btn" >View</button></a>
+
   </div>
- <a href="https://music-player.tamilselvan25.repl.co/" target="_blank"><img src="musicc.jpg" id="audio_img"/><a>
+ 
   </div>
-  <div id="inside_boxes_js">
-  <a href="https://gleeful-cuchufli-b135d1.netlify.app/"  target="_blank"><img src="job.jpg" id="job_Img"/>
+
+
+  <div id="inside_boxes_js" style="background-image:url(job.jpg) ;background-size:100% 100%">
+  <div id="des_div">
+  <p id="des_p">It is a simple job list web app.
+  I used json datas in this project.
+ used to filters 
+  </p>
+  <a href="https://gleeful-cuchufli-b135d1.netlify.app/"  target="_blank"><button   class="btn3 sub_btn" >View</button></a>
   </div>
-  <div id="inside_boxes_js">
-  <a href="https://final-project-js.tamilselvan25.repl.co/"  target="_blank"> <img src="magic_logo.jpg" id="invoice_logo"/></a>
+ 
   </div>
-  <div id="inside_boxes_js">
+
+
+
+  <div id="inside_boxes_js" style="background-image:url(magic_logo.jpg);background-size:100% 100%">
+
+  
+  <div id="des_div">
+  <p id="des_p">It is a magic square.
+ total three levels game
+ 3x3,4x4,5x5 only used passitive numbers used to play
+  </p>
+  <a href="https://final-project-js.tamilselvan25.repl.co/"  target="_blank"> <button   class="btn3 sub_btn" >View</button></a>
+  </div>
+  </div>
+
+  <div id="inside_boxes_js" style="background-image:url(memory_game.png) ;background-size:100% 100%">
   <div id="des_div">
   <p id="des_p">This is  Memorygame .I learned and used to array and objects.
 I used grid properties,floor and random medthos in this game.
   </p>
+  <a href="https://memory-game.tamilselvan25.repl.co/"  target="_blank"> <button   class="btn3 sub_btn" >View</button></a>
+
   </div>
-  <a href="https://memory-game.tamilselvan25.repl.co/"  target="_blank"> <img src="memory_game.png" id="memory_game"/></a>
+  
   </div>
-  <div id="inside_boxes_js">
+  <div id="inside_boxes_js" style="background-image:url(timetrack.jpg) ;background-size:100% 100%">
   <div id="des_div">
   <p id="des_p">This is time tracking page.Totally three filters daily,weekly,monthly.I get and used JSON datas.
   I used to grid properties in this project.
   </p>
+  <a href="https://subtle-pasca-254621.netlify.app/"  target="_blank"> <button   class="btn3 sub_btn" >View</button></a>
+
   </div>
-  <a href="https://subtle-pasca-254621.netlify.app/"  target="_blank"><img src="time track.jpg" </div> id="time_Img"/><a>
+  
   </div>
-  <div id="inside_boxes_js">
+  <div id="inside_boxes_js" style="background-image:url(urlShortLogo.jpg) ;background-size:100% 100%">
   <div id="des_div">
   <p id="des_p">This is a url short page.I used and learned fetch mehod and api.
   </p>
+  <a href="https://statuesque-blancmange-c9b129.netlify.app/"  target="_blank"> <button   class="btn3 sub_btn" >View</button></a>
+
   </div>
-   <a href="https://statuesque-blancmange-c9b129.netlify.app/"  target="_blank"> <img src="urlShortLogo.jpg" id="urlshort"/><a>
-  </div>`;
+
+
+
+     </div>`;
 }
 
 function nodeProjects() {
   let getDiv = document.getElementById("all_contenet");
+
   getDiv.innerHTML = `
   <div id="inside_boxes_node">
   <video src="movie.webm" id="in_video" style="width:100%;height:100%" controls> </video>
@@ -101,6 +150,10 @@ function nodeProjects() {
   </div>
   <div id="inside_boxes_node">
   <a href="https://github.com/Rajalakshmi2005/IP-Address-Tracker"/><img src="ipAddress.jpg" id="en_Img"/><a>
+  </div>
+  <div id="inside_boxes_node">
+  <video src="chat.webm" id="in_video" style="width:100%;height:100%" controls></video>
+
   </div>
 `;
 }
