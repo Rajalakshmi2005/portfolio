7b0c209e80237d67d28fbc984c427aab066384cc
Rajalakshmi Tamilselvan <ts7226987@gmail.com>
2023-03-23 09:29:39 +0530

    new

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function htmlProjects() {
   getDiv.innerHTML = `
   <div id="inside_boxes" style="background-image: url(periadic.png)";>
   <div id="des_div">
+  <p style="color:#0bceaf ;font-size:15px; font-weight:300;">Periodic Table</p>
   <p id="des_p">It is a periodic Table.This project is only Alignments.
   I used to grid properties in this project.
   </p>
@@ -15,6 +16,7 @@ function htmlProjects() {
 
  <div id="inside_boxes" style="background-image: url(bgVideo.png); background-size: 100% 100%;">
  <div id="des_div">
+ <p style="color:#0bceaf ;font-size:15px; font-weight:300;">Background Video</p>
   <p id="des_p">It is a simple background video.
 
   I used to video in this project.
@@ -28,6 +30,7 @@ function htmlProjects() {
 
  <div id="inside_boxes" style="background-image: url(webpage.png); background-size: 100% 100%">
  <div id="des_div">
+ <p style="color:#0bceaf ;font-size:15px; font-weight:300;">Webpage Align</p>
  <p id="des_p">It is a webpage in alignments.
  I learned and used display properties in this project.
  </p>
@@ -39,7 +42,8 @@ function htmlProjects() {
 
  <div id="inside_boxes" style="background-image: url(layouts.png); background-size: 100% 100%;">
  <div id="des_div">
- <p id="des_p">It is a layout alignments project.
+ <p style="color:#0bceaf ;font-size:15px; font-weight:300;">Layouts</p>
+ <p id="des_p">
  I learned and used float and flex properties in this project.
  I learned this projects correct and neet alignments.
  </p>
@@ -52,7 +56,8 @@ function htmlProjects() {
 
  <div id="inside_boxes"  style="background-image:url(planet.jpg); background-size:100% 100%;">
  <div id="des_div">
- <p id="des_p">It is all planet webpage.I learned this page to other pagge nevigation means used a tags and attributes.
+ <p style="color:#0bceaf ;font-size:15px; font-weight:300;">Planets webpage</p>
+ <p id="des_p">I learned this page to other pagge nevigation means used a tags and attributes.
  totally 27 pages nevigation in this project.
  </p>
  <a href="https://final.tamilselvan25.repl.co/"  target="_blank"> <button   class="btn3 sub_btn" >View</button></a>
@@ -69,6 +74,7 @@ function jsProjects() {
   getDiv.innerHTML = `
   <div id="inside_boxes_js" style="background-image:url(musicc.jpg) ;background-size:100% 100%">
   <div id="des_div">
+  <p style="color:#0bceaf ;font-size:15px; font-weight:300;">Music Player</p>
   <p id="des_p">It is a simple music player .
   I used to image in this project.
   I am created play button clicked play this song,pause button,next buutton,previous button
@@ -82,6 +88,7 @@ function jsProjects() {
 
   <div id="inside_boxes_js" style="background-image:url(job.jpg) ;background-size:100% 100%">
   <div id="des_div">
+  <p style="color:#0bceaf ;font-size:15px; font-weight:300;">Job List</p>
   <p id="des_p">It is a simple job list web app.
   I used json datas in this project.
  used to filters 
@@ -97,6 +104,7 @@ function jsProjects() {
 
   
   <div id="des_div">
+  <p style="color:#0bceaf ;font-size:15px; font-weight:300;">Magic Square</p>
   <p id="des_p">It is a magic square.
  total three levels game
  3x3,4x4,5x5 only used passitive numbers used to play
@@ -107,6 +115,7 @@ function jsProjects() {
 
   <div id="inside_boxes_js" style="background-image:url(memory_game.png) ;background-size:100% 100%">
   <div id="des_div">
+  <p style="color:#0bceaf ;font-size:15px; font-weight:300;">Memory Game</p>
   <p id="des_p">This is  Memorygame .I learned and used to array and objects.
 I used grid properties,floor and random medthos in this game.
   </p>
@@ -117,7 +126,9 @@ I used grid properties,floor and random medthos in this game.
   </div>
   <div id="inside_boxes_js" style="background-image:url(timetrack.jpg) ;background-size:100% 100%">
   <div id="des_div">
-  <p id="des_p">This is time tracking page.Totally three filters daily,weekly,monthly.I get and used JSON datas.
+  
+  <p style="color:#0bceaf ;font-size:15px; font-weight:300;">Time Traking</p>
+  <p id="des_p">Totally three filters daily,weekly,monthly.I get and used JSON datas.
   I used to grid properties in this project.
   </p>
   <a href="https://subtle-pasca-254621.netlify.app/"  target="_blank"> <button   class="btn3 sub_btn" >View</button></a>
@@ -127,6 +138,7 @@ I used grid properties,floor and random medthos in this game.
   </div>
   <div id="inside_boxes_js" style="background-image:url(urlShortLogo.jpg) ;background-size:100% 100%">
   <div id="des_div">
+  <p style="color:#0bceaf ;font-size:15px; font-weight:300;">Url Short</p>
   <p id="des_p">This is a url short page.I used and learned fetch mehod and api.
   </p>
   <a href="https://statuesque-blancmange-c9b129.netlify.app/"  target="_blank"> <button   class="btn3 sub_btn" >View</button></a>
